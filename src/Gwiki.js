525ca16e348189e33b7989f87035e5b3d6c26693
Kael Shipman <kael.shipman@cfxtrading.com>
2017-06-07 17:21:53 -0500

    Finished initial implementation of Gwiki functionality

--- src/Gwiki.js
+++ src/Gwiki.js
@@ -2,40 +2,34 @@ Gwiki = function(opts) {
     Utils.merge(this, {
         home : null,
         parents : [],
-        nextItem : null,
         currentItem : null,
-        prevItem : null,
-        mainMenu : [],
-        currentCollection : []
+        mainMenu : []
     }, opts || {});
 }
 
 Gwiki.prototype = Object.create(Object.prototype);
-Gwiki.interface = ['block','init', 'setHome','persist'];
+Gwiki.interface = ['init', 'setHome','persist'];
 
-Gwiki.prototype.block = function(str) {
-    if (!str) str = "Sorry, something went wrong. Please reload the page.";
-    this.ui.block(str);
-}
 
 Gwiki.prototype.init = function() {
     // Sanity checks on init
-    if (!Utils.implements(GwikiUI.interface, this.ui)) throw "You must define a ui rendering engine by passing a 'ui' option in your options hash that implements the GwikiUI interface";
     if (typeof gapi == 'undefined') throw "This library requires a valid gapi instance. Please see https://developers.google.com/drive/v3/web/quickstart/js for information on initializing the gapi javascript client.";
     if (typeof Storage == 'undefined') {
-        gwiki.block('Sorry, your web browser is too old for this. Please upgrade to something more reasonable.');
+        this.dispatchEvent('error', {
+            type : 'IncompatibleEnvironmentError',
+            message : 'Sorry, your web browser is too old for this. Please upgrade to something more reasonable.'
+        });
         return false;
     }
 
     // Restore previous state, if any
     if (typeof localStorage.gwikiState != 'undefined') {
         var state = JSON.parse(localStorage.gwikiState);
-        for (var x in state) this[x] = state;
+        for (var x in state) this[x] = state[x];
     }
 
-    // Draw interface
-    if (this.home === null) this.ui.askForHome();
-    else this.ui.drawStandardInterface();
+    this.initialized = true;
+    this.dispatchEvent('init', { gwiki : this });
 }
 
 Gwiki.prototype.setHome = function(folderId) {
@@ -46,10 +40,22 @@ Gwiki.prototype.setHome = function(folderId) {
         t.home = response.result;
         t.setExtraAttributes(t.home);
         t.getChildren(folderId).then(function(response) {
+            // Create main menu
             t.mainMenu = t.menuize(response.result.files);
-            t.setCurrentItem(t.home, t.mainMenu);
-            t.ui.unblock();
-            t.persist();
+
+            // Remove home document from main menu
+            for (var i = 0; i < t.mainMenu.length; i++) {
+                if (t.mainMenu[i].displayName == t.home.displayName) {
+                    t.mainMenu.splice(i, 1);
+                    break;
+                }
+            }
+
+            // Set current item to home
+            t.setCurrentItem(t.home);
+
+            // Tell listeners that we've set home
+            t.dispatchEvent('setHome', { gwiki : t });
         });
     });
 }
@@ -79,7 +85,7 @@ Gwiki.prototype.setExtraAttributes = function(item) {
     var type = item.mimeType;
     
     // If it's not a folder, then it's a "terminus" page
-    item.isTerminus = (type == 'application/vnd.google-apps.folder');
+    item.isTerminus = (type != 'application/vnd.google-apps.folder');
 
     // See if it's "consumable" by our wiki
     item.isConsumable = (
@@ -111,42 +117,56 @@ Gwiki.prototype.setExtraAttributes = function(item) {
 
 
 Gwiki.prototype.setCurrentItem = function(item) {
+    var t = this;
     if (item.mimeType != 'application/vnd.google-apps.folder') {
         this.currentItem = item;
-        this.displayCurrentItem();
+        if (this.currentItem.isText) {
+            gapi.client.drive.files.get({
+                'fileId' : this.currentItem.id,
+                'alt' : 'media'
+            }).then(function(response) {
+                t.currentItem.body = response.body;
+                t.persist();
+                t.dispatchEvent('setCurrentItem', { gwiki : t });
+            });
+        } else {
+            this.persist();
+            this.dispatchEvent('setCurrentItem', { gwiki : this });
+        }
     } else {
-        var t = this;
+        this.rewindParentsTo(item);
+        this.parents.unshift(item);
         this.getChildren(item.id).then(function(response) {
             var children = t.menuize(response.result.files);
-            var itemSet = false;
+            var selected = false;
+            // If there's a text file of the same name as the folder, that's our item, that should be the menu header and is selected
             for (var i = 0; i < children.length; i++) {
                 if (children[i].displayName == item.displayName) {
-                    t.setCurrentItem(children[i]);
-                    itemSet = true;
+                    selected = children.splice(i,1)[0];
+                    children.unshift(selected);
                     break;
                 }
             }
-            if (!itemSet) t.setCurrentItem(children[0]);
+            // If there's no text file with the same name as the folder, just default to the first item
+            if (!selected) selected = children[0];
+            item.children = children;
+            t.setCurrentItem(selected);
         });
     }
 }
 
 
-
-
-Gwiki.prototype.displayCurrentItem = function() {
-    var t = this;
-    if (this.currentItem.isText) {
-        gapi.client.drive.files.get({
-            'fileId' : this.currentItem.id,
-            'alt' : 'media'
-        }).then(function(response) {
-            t.ui.setMainContent(t.markdownParser.makeHtml(response.body));
-        });
-    } else {
-        this.ui.setMainContent('<iframe class="google-doc" src="https://docs.google.com/document/d/'+this.currentItem.id+'/preview">');
+Gwiki.prototype.rewindParentsTo = function(item) {
+    var selected = false;
+    for (var i = 0; i < this.parents.length; i++) {
+        if (this.parents[i].id == item.id) {
+            selected = i;
+            break;
+        }
     }
+    if (selected !== false) this.parents.splice(selected, (this.parents.length - selected));
 }
+
 
 Gwiki.prototype.getChildren = function(folderId) {
     return gapi.client.drive.files.list({
@@ -163,5 +183,54 @@ Gwiki.prototype.persist = function() {
         currentItem : this.currentItem,
         mainMenu : this.mainMenu
     });
+}
+
+
+
+
+
+
+
+
+
+
+// Event handling
+
+Gwiki.prototype.addEventListener = function(eventName, callback) {
+    if (typeof this.eventListeners == 'undefined') this.eventListeners = {};
+    if (typeof this.eventListeners[eventName] == 'undefined') this.eventListeners[eventName] = [];
+    this.eventListeners[eventName].push(callback);
+}
+
+Gwiki.prototype.removeEventListener = function(eventName, callback) {
+    if (typeof this.eventListeners == 'undefined') this.eventListeners = {};
+    if (typeof this.eventListeners[eventName] == 'undefined') return true;
+
+    for(var i = 0; i < this.eventListeners[eventName].length; i++) {
+        if (this.eventListeners[eventName][i] === callback) {
+            this.eventListeners[eventName].splice(i, 1);
+            break;
+        }
+    }
+    return true;
+}
+
+Gwiki.prototype.dispatchEvent = function(eventName, props) {
+    if (typeof this.eventListeners == 'undefined') this.eventListeners = {};
+    if (typeof this.eventListeners[eventName] == 'undefined') return true;
+
+    var stopPropagation = false;
+    var preventDefault = false;
+    var e = props || {};
+    e.stopPropagation = function() { stopPropagation = true; }
+    e.preventDefault = function() { preventDefault = true; }
+
+
+    for(var i = 0; i < this.eventListeners[eventName].length; i++) {
+        this.eventListeners[eventName][i].call(window, e);
+        if (stopPropagation) break;
+    }
+    
+    return !preventDefault;
 }
 
--- src/GwikiUI.js
+++ src/GwikiUI.js
@@ -6,13 +6,45 @@ GwikiUI = function(opts) {
     }, opts || {});
 
     // Make initial UI
+
+    // Root
     this.root = document.getElementById(this.options.rootId);
+
+
+    // Header
     this.root.appendChild(document.createElement('header'));
     this.header = this.root.lastChild;
     this.header.className = 'gwiki-header';
-    this.root.appendChild(document.createElement('section'));
-    this.main = this.root.lastChild;
-    this.main.className = 'gwiki-main';
+
+    // Title
+    this.header.appendChild(document.createElement('h1'));
+    this.siteTitle = this.header.lastChild;
+    this.siteTitle.className = 'gwiki-site-title';
+
+    // Main navigation
+    this.header.appendChild(document.createElement('nav'));
+    this.mainMenuContainer = this.header.lastChild;
+    this.mainMenuContainer.className = 'gwiki-main-menu';
+    this.mainMenu = [];
+
+
+    // Main Section
+    this.root.appendChild(document.createElement('div'));
+    this.root.lastChild.className = 'gwiki-main';
+
+    // Sub navigation
+    this.root.lastChild.appendChild(document.createElement('nav'));
+    this.subMenuContainer = this.root.lastChild.lastChild;
+    this.subMenuContainer.className = 'gwiki-sub-menu';
+    this.subMenu = [];
+
+    // Main content
+    this.root.lastChild.appendChild(document.createElement('article'));
+    this.mainContent = this.root.lastChild.lastChild;
+    this.mainContent.className = 'gwiki-main-content';
+
+
+    // Footer
     this.root.appendChild(document.createElement('footer'));
     this.footer = this.root.lastChild;
     this.footer.className = 'gwiki-footer';
@@ -20,10 +52,6 @@ GwikiUI = function(opts) {
 
 GwikiUI.prototype = Object.create(Object.prototype);
 GwikiUI.interface = ['block','askForHome','drawStandardInterface'];
-
-GwikiUI.prototype.checkForGwiki = function() {
-    if (!Utils.implements(Gwiki.interface, this.gwiki)) throw "You must provide an instance of Gwiki as the `gwiki` property of a `GwikiUI`";
-}
 
 GwikiUI.prototype.block = function(str) {
     var blocker = document.createElement('div');
@@ -47,11 +75,9 @@ GwikiUI.prototype.unblock = function() {
 
 
 // On first load or when resetting home
-GwikiUI.prototype.askForHome = function() {
-    this.checkForGwiki();
-    var str = '<form action="?" method="GET" class="homeForm"><label for="homeFolder">Home Folder: <input type="text" id="homeFolder" value="'+(this.gwiki.home || '')+'" placeholder="e.g., https://drive.google.com/folders/2gja3lkaw3j-faoejsdlkalgalskdga"> <button type="submit">Ok</button></form>';
+GwikiUI.prototype.askForHome = function(gwiki) {
+    var str = '<form action="?" method="GET" class="homeForm"><label for="homeFolder">Home Folder: <input type="text" id="homeFolder" value="'+(gwiki.home || '')+'" placeholder="e.g., https://drive.google.com/folders/2gja3lkaw3j-faoejsdlkalgalskdga"> <button type="submit">Ok</button></form>';
     var blocker = this.block(str);
-    var gwikiUI = this;
     blocker.getElementsByTagName('form')[0].addEventListener('submit', function(e) {
         e.preventDefault();
 
@@ -60,7 +86,7 @@ GwikiUI.prototype.askForHome = function() {
         var match = folderId.match(/[^a-zA-Z0-9_-]/);
         if (match) folderId = folderId.substr(0, match.index);
 
-        gwikiUI.gwiki.setHome(folderId);
+        gwiki.setHome(folderId);
     });
 }
 
@@ -71,9 +97,78 @@ GwikiUI.prototype.askForHome = function() {
 
 
 
-// On load any page
-GwikiUI.prototype.drawStandardInterface = function() {
-    var body = document.getElementById('gwiki-ui');
+
+// Standard Interface
+
+// Load the interface on init and when home changes
+GwikiUI.prototype.loadStandardInterface = function(gwiki) {
+    // Title
+    document.title = gwiki.home.displayName;
+    this.siteTitle.innerHTML = gwiki.home.displayName;
+    this.siteTitle.addEventListener('click', function(e) {
+        e.preventDefault();
+        gwiki.setCurrentItem(gwiki.home);
+    });
+
+
+    // Main Nav
+    for (var i = 0; i < this.mainMenu.length; i++) { 
+        this.mainMenuContainer.removeChild(this.mainMenu[i]);
+    }
+    this.mainMenu = [];
+
+    for (var i = 0; i < gwiki.mainMenu.length; i++) {
+        this.mainMenu[i] = document.createElement('a');
+        this.mainMenu[i].href = "#"+gwiki.mainMenu[i].id;
+        this.mainMenu[i].innerHTML = gwiki.mainMenu[i].displayName;
+        this.mainMenu[i].setAttribute('data-gid', gwiki.mainMenu[i].id);
+        this.mainMenu[i].gobject = gwiki.mainMenu[i];
+        this.mainMenuContainer.appendChild(this.mainMenu[i]);
+
+        // Click listener
+        this.mainMenu[i].addEventListener('click', function(e) {
+            e.preventDefault();
+            gwiki.setCurrentItem(e.target.gobject);
+        });
+    }
+}
+
+
+
+
+GwikiUI.prototype.updateStandardInterface = function(gwiki) {
+    // Set content
+    if (gwiki.currentItem.body) this.mainContent.innerHTML = this.markdownParser.makeHtml(gwiki.currentItem.body);
+    else this.mainContent.innerHTML = '<iframe class="google-doc" src="https://docs.google.com/document/d/'+gwiki.currentItem.id+'/preview">'
+
+
+    // Select main menu item
+    var selectedMainItem = gwiki.parents[gwiki.parents.length-2];
+    for (var i = 0; i < this.mainMenu.length; i++) {
+        if (selectedMainItem && this.mainMenu[i].gobject.name == selectedMainItem.name) this.mainMenu[i].classList.add('selected');
+        else this.mainMenu[i].classList.remove('selected');
+    }
+
+
+    // Create sub menu
+    this.subMenu = [];
+    this.subMenuContainer.innerHTML = '';
+    if (gwiki.parents.length > 1) {
+        for (var i = 0; i < gwiki.parents[0].children.length; i++) {
+            this.subMenu[i] = document.createElement('a');
+            this.subMenu[i].href = "#"+gwiki.parents[0].children[i].id;
+            this.subMenu[i].innerHTML = gwiki.parents[0].children[i].displayName;
+            this.subMenu[i].setAttribute('data-gid', gwiki.parents[0].children[i].id);
+            this.subMenu[i].gobject = gwiki.parents[0].children[i];
+            this.subMenuContainer.appendChild(this.subMenu[i]);
+
+            // Click listener
+            this.subMenu[i].addEventListener('click', function(e) {
+                e.preventDefault();
+                gwiki.setCurrentItem(e.target.gobject);
+            });
+        }
+    }
 }
 
 
@@ -81,12 +176,31 @@ GwikiUI.prototype.drawStandardInterface = function() {
 
 
 
-GwikiUI.prototype.setMainContent = function(content) {
-    if (typeof content == 'string') this.main.innerHTML = content;
-    else {
-        this.main.innerHTML = '';
-        this.main.appendChild(content);
-    }
+
+
+
+
+GwikiUI.prototype.subscribeListeners = function(gwiki) {
+    var t = this;
+    if (!Utils.implements(Gwiki.interface, gwiki)) throw "You must provide an instance of Gwiki as the argument to this function.";
+    gwiki.addEventListener('error', function(e) { t.block(e.message); });
+    gwiki.addEventListener('init', function(e) {
+        if (e.gwiki.home === null) t.askForHome(e.gwiki);
+        else {
+            t.loadStandardInterface(e.gwiki);
+            t.updateStandardInterface(e.gwiki);
+        }
+    });
+    gwiki.addEventListener('setHome', function(e) {
+        if (e.gwiki.home === null) t.askForHome(e.gwiki);
+        else {
+            t.loadStandardInterface(e.gwiki);
+            t.unblock();
+        }
+    });
+    gwiki.addEventListener('setCurrentItem', function(e) {
+        t.updateStandardInterface(e.gwiki);
+    });
 }
 
 
